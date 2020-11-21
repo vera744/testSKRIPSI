35c9a551038d7f1c3fb16377438612be4f1bd5e5
LuisAlexander10 <luisalexsander10@gmail.com>
2020-11-21 15:46:59 +0700

    Menambahkan template home setelah login

--- resources/views/template/homeLogin.blade.php
+++ resources/views/template/homeLogin.blade.php
@@ -11,6 +11,8 @@
 
         <!-- Styles -->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
         <style>
             html, body {
                 background-color: #fff;
@@ -81,16 +83,13 @@
         </style>
     </head>
     <body>
-    
-  
-
 
     <div class="flex-center position-ref height-header">
         <div class="top-left links">
          
             <a href="/" style="background-color: #e3f2fd;">LOGO</a>
-            <a href="{{ url('/') }}">TATA CARA</a>
-            <a href="{{ url('/') }}" >TENTANG KAMI</a>
+            <a href="{{ url('gadai') }}">GADAI</a>
+            <a href="{{ url('ecom') }}" >E-COMMERCE</a>
             </div>
                 @if (Route::has('login'))
                     
@@ -107,51 +106,12 @@
                     </div>
                 @endif
              </div>
-         </div>
-               
-    </div>
-            
-    <div class="content">
-                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                    <ol class="carousel-indicators">
-                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                    </ol>
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                        <img src="images/gambar1.jp" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="..." class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-                </a>
-                </div>
+         </div>     
     </div>
 
+    <div class="container-fluid mx-3 col-12">
+        @yield('container')
+    </div>
 
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div> 
-        
-      
     </body>
 </html>
