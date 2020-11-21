77353e81517b937b3c665033177507ac0fa62b3f
LuisAlexander10 <luisalexsander10@gmail.com>
2020-11-21 14:01:36 +0700

    tes menampilkan gambar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,7 +120,7 @@
                     </ol>
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img src="{{URL::asset('Image/gambar1.jpg')}}" class="d-block w-100" alt="...">
+                        <img src="images/gambar1.jpg" class="d-block w-100" alt="...">
                         </div>
                         <div class="carousel-item">
                         <img src="..." class="d-block w-100" alt="...">
