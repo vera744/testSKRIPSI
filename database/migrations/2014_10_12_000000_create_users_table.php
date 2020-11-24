af26f4609eb400c3caf8361f39d7a8b904109cea
LuisAlexander10 <luisalexsander10@gmail.com>
2020-11-24 20:23:32 +0700

    menambah variabel di tabel

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -25,6 +25,7 @@ class CreateUsersTable extends Migration
             $table->string("fotodenganKTP");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('role');
             $table->rememberToken();
             $table->timestamps();
         });
