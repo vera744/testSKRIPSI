99df631c76f2914dae54a001547ab6732429a2c4
vera744 <verawu110100@gmail.com>
2020-11-28 18:27:59 +0700

    delete si facades daoble

--- app/Http/Controllers/GadaiController.php
+++ app/Http/Controllers/GadaiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Auth;
 
 class GadaiController extends Controller
