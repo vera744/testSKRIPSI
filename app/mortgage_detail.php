8fde8ad23032057bb356bc28483384a8e5751488
vera744 <verawu110100@gmail.com>
2020-11-28 18:45:40 +0700

    commit mortgage

--- app/mortgage_detail.php
+++ app/mortgage_detail.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class Mortgage_Detail extends Model
 {
     protected $fillable = [
-        'mDetailID', 'mortgageID', 'loan','duration','status'
+        'mDetailID', 'mortgageID', 'loan',
     ];
 
     protected $table = 'mortgage_details';
