6102d410a4e4f0ab84f840181e27d9467ebf6cd8
LuisAlexander10 <luisalexsander10@gmail.com>
2020-12-06 17:05:43 +0700

    product err lol

--- app/Product.php
+++ app/Product.php
@@ -7,8 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     protected $fillable = [
-        'productID', 'productName','productPrice', 'productDetail', 'productQuantity', 'productDescription',
+        'productID', 'productName','productPrice', 'productDetail', 'productQuantity', 'productDescription','fotoProduk'
     ];
 
     protected $table = 'products';
+
+     public function mortgages(){
+      return $this->hasOne('App\Mortgage', 'productID', 'productID');
+    }
 }
