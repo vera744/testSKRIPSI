2a5d5fa9c2fcc3f3c98a3075fb59025cc8bdedf2
LuisAlexander10 <luisalexsander10@gmail.com>
2021-01-20 18:55:29 +0700

    benerin layout

--- resources/views/ecom/pesanview.blade.php
+++ resources/views/ecom/pesanview.blade.php
@@ -1,6 +1,9 @@
 
 @extends('layouts.layoutEcommerce')
-@section('title', "Pesan")
+
+@section('title', "Transaksi Anda")
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+
 
 @section('content')
 @if ($message = Session::get('sukses'))
